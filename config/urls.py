5f07b50e6e2ff2100bc7a51c3a8eb9eb4f592c1d
doyeon <do8343@naver.com>
2021-11-19 01:44:28 +0900

    로그인 페이지 완성

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from codyshare import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.main, name="main"),
+    path('account/', include('account.urls')), #로 축약
 ]
