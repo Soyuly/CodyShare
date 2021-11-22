3c9e19732058e342ad8b5d9eca091a8f1d58009a
soyul <thduf0623@likelion.org>
2021-11-23 01:30:07 +0900

    create

--- codyshare/views.py
+++ codyshare/views.py
@@ -4,6 +4,7 @@ from account.models import Account
 from django.contrib import auth
 import json
 from django.http import JsonResponse
+from .models import Post
 # Create your views here.
 
 
@@ -23,5 +24,22 @@ def mypage(request):
 def create(request):
     return render(request,'create.html')
 
-def detail(request):
+def detail(request,post_id):
     return render(request,'detail.html')
+
+def create_backend(request):
+    user = request.user
+    post = Post()
+    if request.method == "POST":
+        post.user_id = Account.objects.get(id = user.id)
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.img = request.FILES['img']
+        post.gender = request.POST['gender']
+        post.cloth_type = request.POST['cloth_type']
+        post.size = request.POST['size']
+        post.start = request.POST['start']
+        post.end = request.POST['end']
+        post.fee = request.POST['fee']
+        post.save()   
+    return redirect('/detail/' + str(post.id))
