40652ff14ee9b4dc5a86a581a9b2505bbf547089
soyul <thduf0623@likelion.org>
2021-11-23 07:29:20 +0900

    모바일 제한

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s_szpn4jqc7_+rqoun=475y1&(r+*!i(g-7mg0)rs7isa(of-f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['121.156.13.77']
+ALLOWED_HOSTS = []
 
 
 # Application definition
