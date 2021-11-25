834201a1807d3dd588eb74ce4ef46e375f2eb578
doyeon <do8343@naver.com>
2021-11-26 03:48:47 +0900

    메인페이지/마이페이지 css 수정

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,9 +1,12 @@
+console.log("detail.js 실행");
+
 //유효성 검사 로직
 const $inputEelement = $("input, select");  //모든 input 요소를 검사 할 것.
 $(".form__submit").attr("disabled", true);   //일단 submit disabled
 
 //input 에서 벗어날 때마다 검사
-$("input, select").change(function(){
+$("input[type=date]").change(function(){
+    console.log("체인지 실행");
     var re_start=document.getElementsByName('start').value;
     var re_end=document.getElementsByName('end').value;
     re_start = re_start.split('-');
